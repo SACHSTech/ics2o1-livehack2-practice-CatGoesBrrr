49690a77b00bb1caf6ea3d78075077d2b593b4c4
Caitlyn Tan <caitlyn.tan23@ycdsbk12.ca>
2021-02-12 19:22:39 +0000

    Submit

--- problem1.py
+++ problem1.py
@@ -1,1 +1,29 @@
+'''
+-------------------------------------------------------------------------------
+Name:       problem1.py
+Purpose:  Determines Right-angled Triangles
 
+Author:    Tan.C
+
+Created:    date in 02/12/2021
+------------------------------------------------------------------------------
+'''
+print("****** Right-angled Triangles ******")
+
+# get side lengths from user
+side1 = float(input("Enter the length of side 1: "))
+side2 = float(input("Enter the length of side 2: "))
+side3 = float(input("Enter the length of side 3: "))
+
+# side lengths squared
+sq1 = side1**2
+sq2 = side2**2
+sq3 = side3**2
+
+# output results
+print(" ")
+if sq1 + sq2 == sq3 or sq1 + sq3 == sq2 or sq2 + sq3 == sq1:
+  print("The triangle with side lengths " + str(side1) + ", " + str(side2) + ", " + str(side3) + " form a right-angled triangle")
+
+else:
+  print("The triangle with side lengths " + str(side1) + ", " + str(side2) + ", " + str(side3) + " do not form a right-angled triangle")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,25 @@
+'''
+-------------------------------------------------------------------------------
+Name:       problem2.py
+Purpose:  Vacation
 
+Author:    Tan.C
+
+Created:    date in 02/12/2021
+------------------------------------------------------------------------------
+'''
+print("****** Vacation ******")
+
+# get mark and earnings from user
+mark = int(input("Mark: "))
+earnings_before_the_summer = int(input("Earnings: "))
+
+# output results
+if mark >= 80 and earnings_before_the_summer >= 500:
+  print("You can go to Europe!")
+
+elif mark >= 80:
+  print("You can go to California!")
+
+else:
+  print("Sorry, you have to stay home.")
